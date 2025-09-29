695920cfe1453030b6656127aa70acb1f3dabef2
deepblue <seongyun3023@gmail.com>
2025-09-29 11:48:10 +0900

    v0.2 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"strings"
+	"reflect"
 )
 
-func main() {
-	fmt.Println(math.Ceil(2.31))
-	fmt.Println(strings.Title("head first go"))
+//func main() {
+//fmt.Println(math.Ceil(2.31))
+//fmt.Println(strings.Title("head first go"))
+//}
 
+func main() {
+	//var name string
+	// name = "Kim Inha"
+	//var name = "Kim Inha"
+
+	//name := "Kim Inha"
+	var name float32
+	name = 2.71
+	fmt.Println(name, reflect.TypeOf(name))
 }
