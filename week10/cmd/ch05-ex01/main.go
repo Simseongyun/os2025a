847895c176cd820fe09fdb7489f086448b98075e
deepblue <seongyun3023@gmail.com>
2025-11-03 12:08:56 +0900

    week10/array v0.5

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -2,18 +2,14 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	arrayBool := [2]bool{true, false}
-	arrayInt := [3]int{-9, 11, 7}
+
+	numbers := [3]int{-9, 11, 7}
 
 	for i := 0; i < len(arrayInt); i++ {
 		fmt.Println(i, arrayInt[i])
-		//fmt.Println(i, arrayBool[i]) //runtime error
 	}
-	fmt.Println(reflect.TypeOf(arrayBool))
-	//fmt.Printf("%v\n", arrayBool)
-	//fmt.Printf("%v\n", arrayInt)
+
 }
