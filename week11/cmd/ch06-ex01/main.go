ef03c05097945b09ccb1d42e47f1344ef7f542d7
deepblue <seongyun3023@gmail.com>
2025-11-10 11:28:05 +0900

    week11 slice v0.3

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	subjects := []string{"Go", "", "Python"}
+	subjects := []string{"Go", "Javascript", "Python", "Linux"}
+	subjectsSlice := subjects[1:3]
 
 	for _, subjects := range subjects {
 		fmt.Println(subjects)
+	}
+	fmt.Println("=================")
+	for i := 0; i < len(subjectsSlice); i++ {
+		fmt.Println(subjectsSlice[i])
 	}
 }
